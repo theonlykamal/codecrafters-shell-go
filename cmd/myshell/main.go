003c60f492e44c9ff81988871ab5592b358959e5
Krishna Kamal <krishnakamal.code@proton.me>
2024-12-18 16:36:39 +0530

    REPL

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -11,12 +11,15 @@ var _ = fmt.Fprint
 
 func main() {
 
-	fmt.Fprint(os.Stdout, "$ ")
+	for {
+		fmt.Fprint(os.Stdout, "$ ")
 
-	// Wait for user input
-	command, err := bufio.NewReader(os.Stdin).ReadString('\n')
-	if err != nil {
-		return
+		// Wait for user input
+		command, err := bufio.NewReader(os.Stdin).ReadString('\n')
+		if err != nil {
+			return
+		}
+		fmt.Printf("%s: command not found", command[:len(command)-1])
 	}
-	fmt.Printf("%s: command not found", command[:len(command)-1])
+
 }
